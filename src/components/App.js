ddf98cebabdb8ddba969e5f5244796f9a3f9f02c
Pavel Sakun <axelhomkin@gmail.com>
2021-03-02 10:48:22 +0200

    created react app

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import style from './App.module.css';
+import { v4 as uuidv4 } from 'uuid';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return <div > < /div>;
+    }
+}
+
+export default App;
